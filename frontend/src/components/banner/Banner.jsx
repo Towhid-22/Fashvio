982988c39ececb6da75e8c37e7c10a0e87f06333
Towhidul Islam <towhid212037@gmail.com>
2025-12-21 21:41:03 +0600

    banner fetched

--- frontend/src/components/banner/Banner.jsx
+++ frontend/src/components/banner/Banner.jsx
@@ -1,5 +1,5 @@
 "use client";
-import * as React from "react";
+import React, { useState, useEffect } from "react";
 import { useRef } from "react";
 import Autoplay from "embla-carousel-autoplay";
 import {
@@ -10,8 +10,21 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 import { banners } from "../../../public";
+import axios from "axios";
+import Link from "next/link";
 
 const Banner = () => {
+  const [banners, setBanners] = useState([]);
+  useEffect(() => {
+    function getBanner() {
+      axios
+        .get(`${process.env.NEXT_PUBLIC_URL}/api/banner/get-banner`)
+        .then((res) => {
+          setBanners(res.data.data);
+        });
+    }
+    getBanner();
+  }, []);
   const plugin = useRef(Autoplay({ delay: 3000, stopOnInteraction: false }));
   return (
     <Carousel
@@ -23,13 +36,15 @@ const Banner = () => {
       <CarouselContent>
         {banners.map((item, index) => (
           <CarouselItem key={index} className="w-full">
-            <div className="py-3">
-              <img
-                src={item.image}
-                alt={item.name}
-                className="w-full h-40 lg:h-[600px] object-contain rounded-[5px]"
-              />
-            </div>
+            <Link href={`/${item.href}`}>
+              <div className="py-3">
+                <img
+                  src={item.image}
+                  alt={item.name}
+                  className="w-full h-40 lg:h-[600px] object-contain rounded-[5px]"
+                />
+              </div>
+            </Link>
           </CarouselItem>
         ))}
       </CarouselContent>
--- frontend/src/components/category/Category.jsx
+++ frontend/src/components/category/Category.jsx
@@ -1,20 +1,39 @@
-import React from "react";
-import { categories } from "../../../public";
+"use client";
+import React, { useEffect, useState } from "react";
+// import { categories } from "../../../public";
+import axios from "axios";
+import Link from "next/link";
 
 const Category = () => {
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    function categoryList() {
+      axios
+        .get(`${process.env.NEXT_PUBLIC_URL}/api/category/get-category`)
+        .then((res) => {
+          setCategories(res.data.data);
+        });
+    }
+    categoryList();
+  }, []);
   return (
     <div className="mx-auto max-w-[1580px] px-4 mt-6">
+      <h1 className="font-lato  sm:text-3xl font-semibold mb-6 text-textPrimary">
+        All Category
+      </h1>
       <div className="grid grid-cols-2 sm:grid-cols-5 lg:grid-cols-7 xl:grid-cols-10 2xl:grid-cols-12 gap-4 place-items-center">
         {categories.map((item) => (
-          <div
-            key={item.id}
-            className="shadow p-3 flex flex-col items-center w-30 h-30 justify-center rounded-[5px] cursor-pointer hover:bg-primaryColor/20 transition-all duration-300"
-          >
-            <img src={item.image} alt={item.name} className="w-12 h-12" />
-            <p className="text-textprimaryColor font-quicksand font-semibold mt-1">
-              {item.name}
-            </p>
-          </div>
+          <Link href={`/shop/${item.slug}`}>
+            <div
+              key={item.id}
+              className="shadow p-3 flex flex-col items-center w-30 h-30 justify-center rounded-[5px] cursor-pointer hover:bg-primaryColor/20 transition-all duration-300"
+            >
+              <img src={item.image} alt={item.name} className="w-12 h-12" />
+              <p className="text-textprimaryColor font-quicksand font-semibold mt-1">
+                {item.name}
+              </p>
+            </div>
+          </Link>
         ))}
       </div>
     </div>
