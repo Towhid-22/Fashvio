277ea703264c5532d04e585b1d3ff9a80c6ea9d7
Towhidul Islam <towhid212037@gmail.com>
2026-02-01 08:16:13 +0600

    logout and authchek functionality done

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -134,7 +134,7 @@ const loginController = async (req, res) => {
             // });
             // res.cookie("fashvio", token);
             if (existingUser.role == "admin") {
-              req.session.cookie.maxAge = 24 * 60 * 60 * 1000;
+              req.session.cookie.maxAge = 5 * 60 * 1000;
             } else {
               req.session.cookie.maxAge = 5 * 24 * 60 * 60 * 1000;
             }
