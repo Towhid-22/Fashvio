46fd3d6a7b67287f184faca0db357b715cc6f571
Towhidul Islam <towhid212037@gmail.com>
2026-04-04 10:41:13 +0600

    category create

--- backend/controller/categoryController.js
+++ backend/controller/categoryController.js
@@ -101,7 +101,6 @@ const deleteCategoryController = async (req, res) => {
         return res.status(200).json({
           success: true,
           message: "Category deleted successfully",
-          data: deleteCategory,
         });
       }
     });
