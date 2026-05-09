33e029647d0857dc5fdd10c12a5038a97cf420e8
Towhidul Islam <towhid212037@gmail.com>
2026-05-09 07:30:38 +0600

    some update in cart

--- backend/controller/cartController.js
+++ backend/controller/cartController.js
@@ -44,6 +44,7 @@ const updateCartController = async (req, res) => {
     const { id } = req.params;
     const { quantity } = req.body;
     const cart = await cartModel.findById(id).populate("variant product");
+    return console.log(cart.product.quantity);
     if (cart.variant) {
       if (cart.variant.stock < quantity) {
         return res
@@ -66,20 +67,26 @@ const updateCartController = async (req, res) => {
           .json({ success: false, message: "You can only update your cart" });
       }
     } else {
-      if (req.user.id == cart.user) {
-        await cartModel.findOneAndUpdate(
-          { _id: id },
-          { quantity },
-          { new: true },
-        );
-        return res.status(200).json({
-          success: true,
-          message: "Cart updated successfully",
-        });
+      if (cart.product.quantity > quantity) {
+        if (req.user.id == cart.user) {
+          await cartModel.findOneAndUpdate(
+            { _id: id },
+            { quantity },
+            { new: true },
+          );
+          return res.status(200).json({
+            success: true,
+            message: "Cart updated successfully",
+          });
+        } else {
+          return res
+            .status(400)
+            .json({ success: false, message: "You can only update your cart" });
+        }
       } else {
         return res
           .status(400)
-          .json({ success: false, message: "You can only update your cart" });
+          .json({ success: false, message: "Stock not available" });
       }
     }
   } catch (error) {
