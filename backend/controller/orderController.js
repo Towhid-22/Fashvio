735df544d3fdbc92ed35dc5108a86819f7685775
Towhidul Islam <towhid212037@gmail.com>
2026-06-21 18:06:47 +0600

    sslcommerz processing

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -1,5 +1,11 @@
 const cartModel = require("../model/cartModel");
 const orderModel = require("../model/orderModel");
+const userModel = require("../model/userModel");
+
+const SSLCommerzPayment = require("sslcommerz-lts");
+const store_id = process.env.SSL_STORE_ID;
+const store_passwd = process.env.SSL_STORE_PASSWORD;
+const is_live = false; //true for live, false for sandbox
 
 const orderController = async (req, res) => {
   try {
@@ -46,10 +52,51 @@ const orderController = async (req, res) => {
           data: newOrder,
         });
       } else {
-        return res.status(200).json({
-          success: true,
-          message: "Order Place Successfull, Please proceed to payment",
+        // handle online payment logic here
+        let userInfo = await userModel.findById(user);
+        console.log("line no 57", userInfo);
+        return;
+        const data = {
+          total_amount: totalPrice,
+          currency: "BDT",
+          tran_id: `REF${Date.now()}`, // use unique tran_id for each api call
+          success_url: "http://localhost:3030/success",
+          fail_url: "http://localhost:3030/fail",
+          cancel_url: "http://localhost:3030/cancel",
+          ipn_url: "http://localhost:3030/ipn",
+          shipping_method: "Courier",
+          product_name: "Computer.",
+          product_category: "Electronic",
+          product_profile: "general",
+          cus_name: userInfo.username,
+          cus_email: userInfo.email,
+          cus_add1: address,
+          cus_add2: "Dhaka",
+          cus_city: city,
+          cus_state: "Dhaka",
+          cus_postcode: "1000",
+          cus_country: "Bangladesh",
+          cus_phone: phone,
+          cus_fax: "01711111111",
+          ship_name: "Customer Name",
+          ship_add1: "Dhaka",
+          ship_add2: "Dhaka",
+          ship_city: "Dhaka",
+          ship_state: "Dhaka",
+          ship_postcode: 1000,
+          ship_country: "Bangladesh",
+        };
+        const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live);
+        sslcz.init(data).then((apiResponse) => {
+          // Redirect the user to payment gateway
+          let GatewayPageURL = apiResponse.GatewayPageURL;
+          res.redirect(GatewayPageURL);
+          console.log("Redirecting to: ", GatewayPageURL);
         });
+        // return res.status(200).json({
+        //   success: true,
+        //   message: "Order Place Successfull, Please proceed to payment",
+        // });
       }
     }
   } catch (error) {
