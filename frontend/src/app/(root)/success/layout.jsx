6193c08ec7b2a0db2a6ae8ceea202dea9983cc98
Towhidul Islam <towhid212037@gmail.com>
2026-06-22 10:57:58 +0600

    succes route create

---
+++ frontend/src/app/(root)/success/layout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const metadata = {
+  title: "Success",
+  description: "Success Page",
+};
+const layout = ({ children }) => {
+  return <div>{children}</div>;
+};
+
+export default layout;
